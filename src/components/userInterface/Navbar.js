a7c9d28c06f4f6155faa6ef4202a8fe481108746
jelsync <jelsync@gmail.com>
2021-01-18 16:38:31 -0600

    Navbar
    Navbar implementation

--- src/components/userInterface/Navbar.js
+++ src/components/userInterface/Navbar.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar ">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand" >Metro WorkShop</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        <a class="nav-link" href="#">Features</a>
-                        <a class="nav-link" href="#">Pricing</a>
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
+                        <Link to="/BedroomScreen" className="nav-link" >Bedroom</Link>
+                        <Link to="/KitchenScreen" className="nav-link" >Kitchen</Link>
+                        <Link to="/RoomScreen" className="nav-link" >Room</Link>
+                        <Link to="OthersScreen" className="nav-link" >Others</Link>
                     </div>
                 </div>
             </div>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,7 +3,10 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect,
 } from "react-router-dom";
+
+import { HomeArticlesScreen } from "../components/articles/HomeArticlesScreen";
 import { BedroomScreen } from "../components/bedroom/BedroomScreen";
 import { BuyScreen } from "../components/buy/BuyScreen";
 import { KitchenScreen } from "../components/kitchen/KitchenScreen";
@@ -11,21 +14,25 @@ import { CreateUserScreen } from "../components/login/CreateUserScreen";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { OthersScreen } from "../components/others/OthersScreen";
 import { RoomScreen } from "../components/room/RoomScreen";
+import { Navbar } from "../components/userInterface/Navbar";
 
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
-
+                <Navbar/>
                 <Switch>
-                    <Route path="/BedroomScreen" component={ BedroomScreen }/>
-                    <Route path="/BuyScreen" component={ BuyScreen }/>
-                    <Route path="/KitchenScreen" component={ KitchenScreen }/>
-                    <Route path="/CreateUserScreen" component={ CreateUserScreen }/>
-                    <Route path="/LoginScreen" component={ LoginScreen }/>
-                    <Route path="/RoomScreen" component={ RoomScreen }/>
-                    <Route path="/OthersScreen" component={ OthersScreen }/>
+                    <Route exact path="/" component={ HomeArticlesScreen }/>
+                    <Route exact path="/BedroomScreen" component={ BedroomScreen }/>
+                    <Route exact path="/BuyScreen" component={ BuyScreen }/>
+                    <Route exact path="/KitchenScreen" component={ KitchenScreen }/>
+                    <Route exact path="/RoomScreen" component={ RoomScreen }/>
+                    <Route exact path="/OthersScreen" component={ OthersScreen }/>
+                    <Route exact path="/LoginScreen" component={ LoginScreen }/>
+                    <Route exact path="/CreateUserScreen" component={ CreateUserScreen }/>
+
+                    <Redirect to="/"/>
 
                 </Switch>
             </div>
