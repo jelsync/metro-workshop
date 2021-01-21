e6f90664f59f89e30c68cd684e243541f4e61a06
jelsync <jelsync@gmail.com>
2021-01-21 01:24:35 -0600

    CSS style to kitchen, bedroom, room and other templates
    Layout of three client-side templates

--- src/components/others/OthersScreen.js
+++ src/components/others/OthersScreen.js
@@ -2,8 +2,50 @@ import React from 'react'
 
 export const OthersScreen = () => {
     return (
-        <div>
-            <h1>OthersScreen</h1>
-        </div>
+        <>
+            <div className="container mt-2">
+                <h1 className="display-6">Others Screen</h1>
+                <div className="row justify-content-end">
+                    <div className="col col-lg-4 col-md-4 col-sm-4 home-area">
+                        <div className="card">
+                            <div className="card-body">
+                                <h4 className="card-title">Card title</h4>
+                                <svg xmlns="" className="d-block user-select-none" width="100%" height="150" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" />
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a to="#" className="card-link">Card link</a>
+                                <a to="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col col-lg-4 col-md-4 col-sm-4 home-area">
+                        <div className="card">
+                            <div className="card-body">
+                                <h4 className="card-title">Card title</h4>
+                                <svg xmlns="" className="d-block user-select-none" width="100%" height="150" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" />
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a to="#" className="card-link">Card link</a>
+                                <a to="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col col-lg-4 col-md-4 col-sm-4 home-area">
+                        <div className="card">
+                            <div className="card-body">
+                                <h4 className="card-title">Card title</h4>
+                                <svg xmlns="" className="d-block user-select-none" width="100%" height="150" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" />
+                                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a to="#" className="card-link">Card link</a>
+                                <a to="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
