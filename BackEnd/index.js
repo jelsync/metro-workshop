71679352a41ddeecca95d727d223674d99c8f004
jelsync <jelsync@gmail.com>
2021-01-23 19:20:23 -0600

    Creating the routes related to users
    Del lado de backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,12 +3,18 @@ require('dotenv').config();
 
 const app = express();
 
+//Direcction public
 app.use( express.static(' public '));
 
-// app.get('/', (req,res)=>{
+//Router
+app.use('/api/auth', require('./routes/auth'));
 
-// })
+//Auth
 
+//CRUD
+
+
+//lisen request
 app.listen(process.env.PORT, ()=>{
     console.log(`Active Server`);
 });
